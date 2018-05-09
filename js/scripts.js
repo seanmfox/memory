9d436046b9229652042b2ce795261a5d0d1c0956
Sean Fox <geekypghrunner@gmail.com>
2018-05-09 13:54:26 +0000

    fix: Prevent user from clicking during CSS transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
+//Game load global variables
 const grid = document.querySelector('.grid');
-let firstChoice = "";
-let secondChoice = "";
-let firstChoiceClasses = "";
-let secondChoiceClasses = "";
-let start = "00:00:00";
-let incrementTimer = "";
+const timer = document.querySelector('.timer');
+let firstChoice = '';
+let secondChoice = '';
+let firstChoiceClasses = '';
+let secondChoiceClasses = '';
+let start = '00:00:00';
+let incrementTimer = '';
 
 function loadGame() {
-    let shapes = ['<i class="far fa-bell icon"></i>','<i class="fas fa-arrows-alt"></i>','<i class="fas fa-battery-full"></i>','<i class="far fa-calendar"></i>','<i class="fas fa-bicycle"></i>','<i class="fas fa-camera"></i>','<i class="fas fa-clock"></i>','<i class="fas fa-football-ball"></i>','<i class="far fa-bell icon"></i>','<i class="fas fa-arrows-alt"></i>','<i class="fas fa-battery-full"></i>','<i class="far fa-calendar"></i>','<i class="fas fa-bicycle"></i>','<i class="fas fa-camera"></i>','<i class="fas fa-clock"></i>','<i class="fas fa-football-ball"></i>'];
-    firstChoice = "";
-    secondChoice = "";
-    firstChoiceClasses = "";
-    secondChoiceClasses = "";
+    let shapes = ['<i class="far fa-bell icon"></i>',
+        '<i class="fas fa-arrows-alt"></i>',
+        '<i class="fas fa-battery-full"></i>',
+        '<i class="far fa-calendar"></i>',
+        '<i class="fas fa-bicycle"></i>',
+        '<i class="fas fa-camera"></i>',
+        '<i class="fas fa-clock"></i>',
+        '<i class="fas fa-football-ball"></i>',
+        '<i class="far fa-bell icon"></i>',
+        '<i class="fas fa-arrows-alt"></i>',
+        '<i class="fas fa-battery-full"></i>',
+        '<i class="far fa-calendar"></i>',
+        '<i class="fas fa-bicycle"></i>',
+        '<i class="fas fa-camera"></i>',
+        '<i class="fas fa-clock"></i>',
+        '<i class="fas fa-football-ball"></i>'
+    ];
+    firstChoice = '';
+    secondChoice = '';
+    firstChoiceClasses = '';
+    secondChoiceClasses = '';
     choiceCount = 0;
     attempts = 0;
     correctMatch = 0;
@@ -30,7 +48,6 @@ function loadGame() {
     grid.appendChild(fragment);
     
     //Start timer
-    const timer = document.querySelector('.timer');
     start = Date.now();
     incrementTimer = window.setInterval(function(){
         let timing = Math.floor((Date.now() - start)/1000); 
@@ -38,21 +55,23 @@ function loadGame() {
     }, 1000);
     
     //Update timer display
-    function timeFormat(current) {
+    const timeFormat = function(current) {
         let seconds = ((current % 60));
         let minutes = (Math.floor((current / 60)) - (Math.floor((current / 3600)) * 60));
         let hours = Math.floor((current / 3600));
         timer.innerHTML = hours.toString().padStart(2,'0') + ':' + minutes.toString().padStart(2,'0') + ':' + seconds.toString().padStart(2,'0');
-    }
+    };
 }
 
+//Click event global variables
+let attemptCount = document.querySelector('.counter');
 let choiceCount = 0;
 let attempts = 0;
-let attemptCount = document.querySelector('.counter');
 let correctMatch = 0;
 
 grid.addEventListener("click", function(e){
-    if(e.target.firstElementChild.classList[0] !== "card") {
+    if(e.target.firstElementChild !== null && e.target.firstElementChild.classList[0] !== 'card' && 
+    (secondChoiceClasses === '' || !secondChoice.parentElement.classList.contains('wrong'))) {
         choiceCount += 1;
         if(choiceCount === 1){
             firstChoice = e.target.firstElementChild;
@@ -70,18 +89,18 @@ grid.addEventListener("click", function(e){
                 secondChoice.parentElement.classList.add('correct');
                 correctMatch += 1;
                 choiceCount = 0;
-                if (correctMatch === 8) {
+                if (correctMatch === 1) {
                     console.log("You won!");
-                    window.clearInterval(incrementTimer);
+                    gameWin();
                 }
             } else {
                 firstChoice.parentElement.classList.add('wrong');
-                secondChoice.parentElement.classList.add('wrong');                
+                secondChoice.parentElement.classList.add('wrong');
                 window.setTimeout(function() {
+                    firstChoiceClasses.remove('chosen');
+                    secondChoiceClasses.remove('chosen');
                     firstChoice.parentElement.classList.remove('wrong');
                     secondChoice.parentElement.classList.remove('wrong'); 
-                    firstChoiceClasses.remove('chosen', 'wrong');
-                    secondChoiceClasses.remove('chosen', 'wrong');
                     }, 1000);
                 choiceCount = 0;
             }
@@ -89,11 +108,28 @@ grid.addEventListener("click", function(e){
     }
 });
 
-loadGame();
-
-const reset = document.querySelector('.reset');
-reset.addEventListener("click", function(e){
-    grid.innerHTML = "";
+//Reset game
+const gameReset = function(){
+    console.log('resetting');
+    grid.innerHTML = '';
     loadGame();
     attemptCount.textContent = 0;
-})
+    congrats.classList.remove('win');
+};
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', gameReset);
+
+//Game win
+const gameWin = function(){
+    window.setTimeout(function() {
+        congrats.classList.add('win');
+    }, 1000);
+    window.clearInterval(incrementTimer);
+    finalTime.textContent = timer.textContent;
+};
+const congrats = document.querySelector('.congrats');
+const finalTime = document.querySelector('.final-time');
+const replay = document.querySelector('.replay');
+replay.addEventListener('click', gameReset);
+
+loadGame();
